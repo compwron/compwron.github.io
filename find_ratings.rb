bb2e7eca4b5c7b92e473e32df84ae9ebc4e82b01
compwron <compiledwrong@gmail.com>
2015-04-12 16:47:33 -0700

    script and typofix

--- find_ratings.rb
+++ find_ratings.rb
@@ -1,14 +1,28 @@
+class Rater
+  attr_accessor :order
 
-def sorter foo
-	['backmarch', 'step', 'stroll', 'hike', 'climb', 'fly'].index foo || -1
+  def initialize data=nil
+    lines = data || `grep '*' _posts/2015-04-07-pycon.md`
+    @order = _order(lines)
+  end
+
+  def sorter i
+    clean_i = i.gsub('[LT]', '').strip.downcase
+    ['backmarch', 'step', 'stroll', 'hike', 'climb', 'fly'].index(clean_i) || -1
+  end
+
+  def _order(lines)
+    lines.split("\n").map {|l| 
+      l.split('*')
+    }.compact.sort_by {|i| 
+      sorter(i.last) 
+    }.reverse.map {|i| 
+      i.last + "----" + i.first
+    }
+  end
 end
 
-lines = `grep '*' _posts/2015-04-07-pycon.md`
-p lines
-puts lines.split("\n").map {|l| 
-	l.split('*')
-}.compact.sort_by {|i| 
-	sorter(i.last.strip.downcase) 
-}.reverse.map {|i| 
-	i.last + "----" + i.first
-}
+if __FILE__==$0
+  $:.unshift File.expand_path("../../", __FILE__)  
+  puts (Rater.new).order
+end
